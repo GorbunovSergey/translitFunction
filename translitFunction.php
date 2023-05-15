4c79904a47aa0944e34e2bc53873626f71e596e1
GorbunovSergey <anabiosis56@yandex.ru>
2023-05-15 11:30:47 +0300

    Добавлена форма для ввода числа, исправлена ошибка с числами от 10 до 19.

--- translitFunction.php
+++ translitFunction.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
 
 function translit($number)
 {
-    $num = strval($number);
-    $numArr = str_split($num, 1);
+
+    $numArr = str_split($number, 1);
+
+
 
     $units = [1 => 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
-    $eleven = [1 => 'одиннадцать', 'двеннадцать', 'триннадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
+    $eleven = [1 => 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
     $dozens = [1 => 'десять', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто',];
     $hundreds = [1 => 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
 
@@ -19,7 +21,7 @@ function translit($number)
                     echo $val;
                 }
             }
-        break;
+            break;
 
         case 2:
             if ($numArr[0] == 1 && $numArr[1] < 10) {
@@ -44,8 +46,8 @@ function translit($number)
                 }
             }
 
-        break;
-        
+            break;
+
         case 3:
 
             foreach ($hundreds as $key => $val) {
@@ -53,17 +55,41 @@ function translit($number)
                     echo $val . ' ';
                 }
             }
-            foreach ($dozens as $key => $val) {
-                if ($numArr[1] == $key) {
-                    echo $val . ' ';
+
+            if ($numArr[1] > 1) {
+                foreach ($dozens as $key => $val) {
+                    if ($numArr[1] == $key) {
+                        echo $val . ' ';
+                    }
                 }
-            }
-            foreach ($units as $key => $val) {
-                if ($numArr[2] == $key) {
-                    echo $val;
+                foreach ($units as $key => $val) {
+                    if ($numArr[2] == $key) {
+                        echo $val;
+                    }
+                }
+            } else {
+                foreach ($eleven as $key => $val) {
+                    if ($numArr[2] == $key) {
+                        echo $val . ' ';
+                    }
                 }
             }
 
             break;
     }
 }
+
+
+?>
+<?php if (empty($_GET)) { ?>
+    <form action="" method="GET">
+        <input name="num">
+        <input type="submit">
+    </form>
+<?php } else {
+
+    $num = $_GET['num'];
+
+    echo translit($num);
+}
+?>
